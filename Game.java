4d2c4daf72b9e40e1dc46fb56fac89d6593dc9f3
tl3119 <tl3119@nyu.edu>
2023-04-24 11:00:27 -0400

    set graphics

--- Game.java
+++ Game.java
@@ -1,2 +1,40 @@
-public class Game {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import java.io.*;
+
+public class Game{
+    public static void main(String[] args) throws IOException {
+        Gomo g = new Gomo();
+        g.setUp();
+    }
+}
+
+class Gomo{
+    static final int boardSize = 15;
+    static final int cellSize = 35;
+    JFrame jf;
+    JPanel boardPanel;
+    Container contentPane;
+
+    void setUp() {
+        jf = new JFrame("Gomoku Game!");
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setSize(boardSize * cellSize, boardSize * cellSize);
+        jf.setLocationRelativeTo(null);
+
+        // Initialize the board panel
+        boardPanel = new JPanel(new GridLayout(boardSize, boardSize));
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                JButton button = new JButton();
+                button.setPreferredSize(new Dimension(cellSize, cellSize));
+                boardPanel.add(button);
+            }
+        }
+
+        jf.add(boardPanel);
+        jf.setVisible(true);
+    }
 }
