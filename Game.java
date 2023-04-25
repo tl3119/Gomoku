36cb78997ee9d106e430cbbabd5b7a8ff7a12441
AndyisTianyiZhang <tz2020@nyu.edu>
2023-04-25 18:06:51 -0400

    andy

--- Game.java
+++ Game.java
@@ -1,52 +1,89 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.net.*;
-import java.io.*;
 
-public class Game{
-    public static void main(String[] args) throws IOException {
-        Gomo g = new Gomo();
-        g.setUp();
+public class Game {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Gomo g = new Gomo();
+                g.setUp();
+            }
+        });
     }
+
+    static ImageIcon blackStone;
+    static ImageIcon whiteStone;
 }
 
-class Gomo{
+class Gomo {
     static final int boardSize = 15;
     static final int cellSize = 32;
     JFrame jf;
-    JPanel boardPanel;
-    Container contentPane;
+    BoardPanel boardPanel;
 
     void setUp() {
         jf = new JFrame("Gomoku Game!");
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jf.setSize(boardSize * cellSize, boardSize * cellSize);
+        jf.setSize(boardSize * cellSize + 1, boardSize * cellSize + 1);
         jf.setLocationRelativeTo(null);
 
+        // Load stone images
+        Game.blackStone = new ImageIcon("images/black_stone.png");
+        Game.whiteStone = new ImageIcon("images/white_stone.png");
+
         // Initialize the board panel
-        boardPanel = new JPanel(new GridLayout(boardSize, boardSize));
-        for (int i = 0; i < boardSize; i++) {
-            for (int j = 0; j < boardSize; j++) {
-                JButton button = new JButton();
-                button.setPreferredSize(new Dimension(cellSize, cellSize));
-                button.addActionListener(new buttonPressed());
-                boardPanel.add(button);
-            }
-        }
+        boardPanel = new BoardPanel();
+        boardPanel.addMouseListener(new BoardClickListener());
 
         jf.add(boardPanel);
         jf.setVisible(true);
     }
 }
 
-class buttonPressed implements ActionListener{
+class BoardPanel extends JPanel {
+    int[][] board;
+
+    public BoardPanel() {
+        super();
+        board = new int[Gomo.boardSize][Gomo.boardSize];
+    }
+
     @Override
-    public void actionPerformed(ActionEvent e){
-        JButton source = (JButton) e.getSource();
-        source.setOpaque(true);
-        source.setContentAreaFilled(true);
-        source.setBorderPainted(false);
-        source.setBackground(Color.BLACK);
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int gridSize = Gomo.boardSize * Gomo.cellSize;
+        g.setColor(Color.BLACK);
+        for (int i = 0; i <= Gomo.boardSize; i++) {
+            g.drawLine(i * Gomo.cellSize, 0, i * Gomo.cellSize, gridSize);
+            g.drawLine(0, i * Gomo.cellSize, gridSize, i * Gomo.cellSize);
+        }
+
+        for (int i = 0; i < Gomo.boardSize; i++) {
+            for (int j = 0; j < Gomo.boardSize; j++) {
+                if (board[i][j] == 1) {
+                    g.drawImage(Game.blackStone.getImage(), i * Gomo.cellSize - Game.blackStone.getIconWidth() / 2, j * Gomo.cellSize - Game.blackStone.getIconHeight() / 2, null);
+                } else if (board[i][j] == 2) {
+                    g.drawImage(Game.whiteStone.getImage(), i * Gomo.cellSize - Game.whiteStone.getIconWidth() / 2, j * Gomo.cellSize - Game.whiteStone.getIconHeight() / 2, null);
+                }
+            }
+        }
+    }
+}
+
+class BoardClickListener extends MouseAdapter {
+    private boolean blackTurn = true;
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int x = (e.getX() + Gomo.cellSize / 2) / Gomo.cellSize;
+        int y = (e.getY() + Gomo.cellSize / 2) / Gomo.cellSize;
+
+        if (((BoardPanel) e.getSource()).board[x][y] == 0) {
+            int stoneValue = blackTurn ? 1 : 2;
+            ((BoardPanel) e.getSource()).board[x][y] = stoneValue;
+            blackTurn = !blackTurn;
+            ((BoardPanel) e.getSource()).repaint();
+        }
     }
 }
